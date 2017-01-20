761f43885383195347dbd8aacc1e8595c6b9c121
pedall <peterdodell@icloud.com>
2017-01-20 02:24:18 +0100

    OK

--- bot.js
+++ bot.js
@@ -28,6 +28,7 @@ const config = require("./Configuration/config.json");
 const winston = require("winston");
 const domain = require("domain");
 
+
 // Set up default winston logger
 winston.add(winston.transports.File, {
 	filename: "bot-out.log"
