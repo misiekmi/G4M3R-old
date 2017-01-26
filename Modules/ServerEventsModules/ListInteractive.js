bfaa23e55a9d1fb4930ac6cd6b085241f7ff7144
nate mathews <njm3308@rit.edu>
2017-01-25 21:10:23 -0500

    tried adding an empty page at the end of pages since the app seems to error only when it hits the final page.

--- Modules/ServerEventsModules/ListInteractive.js
+++ Modules/ServerEventsModules/ListInteractive.js
@@ -18,6 +18,9 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
     if(new_page.length>0)
         pages.push(new_page);
 
+    pages.push([])
+
+    let real_page_size = pages.length+1;
     let current_page_no = 1;
 
     let getPage = (page_no) => {
@@ -30,14 +33,14 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
 
         description += "\n";
         
-        if(pages.length>1 && page_no<pages.length )
+        if(real_page_size>1 && page_no<real_page_size )
             description += `\`\`[${max_page_size+1}]\`\` **Go to next page**\n`;
         if(page_no>1)
             description += `\`\`[${max_page_size+2}]\`\` **Return to previous page**\n`;
 
         description += `\`\`[cancel]\`\` **Exit view**\n`;
 
-        return {embed: {description: description, footer: {text: `page ${page_no}/${pages.length}`}}}
+        return {embed: {description: description, footer: {text: `page ${page_no}/${real_page_size}`}}}
     };
 
     let embed = getPage(current_page_no);
@@ -53,10 +56,10 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
             msg.channel.createMessage(embed).then(bot_message => {
                 winston.info(`Current page no: '${current_page_no}'`, {srvrid: serverDocument._id});
                 bot.awaitMessage(msg.channel.id, msg.author.id, usr_message => {
-                    //if (usr_err) {
-                    //    err_msg.delete();
-                    //    usr_err = false;
-                    //}
+                    if (usr_err) {
+                        err_msg.delete();
+                        usr_err = false;
+                    }
 
                     let usr_input = usr_message.content.trim();
                     winston.info(`User input: '${usr_input}'`, {srvrid: serverDocument._id});
@@ -66,7 +69,7 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
                         // TODO
                     }
                     // go to next page
-                    else if (usr_input == max_page_size+1 && current_page_no<pages.length) {
+                    else if (usr_input == max_page_size+1 && current_page_no<real_page_size) {
                         current_page_no++;
                         embed = getPage(current_page_no);
                     }
@@ -80,12 +83,9 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
                         cancel = false;
                     }
                     // error
-                    //else {
-                    //    err_msg = msg.channel.createMessage("That's not an option! Please try again.");
-                    //    usr_err = true;
-                    //}
                     else {
-                        cancel = false;
+                        err_msg = msg.channel.createMessage("That's not an option! Please try again.");
+                        usr_err = true;
                     }
 
                     bot_message.delete();
