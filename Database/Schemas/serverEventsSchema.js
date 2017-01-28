bcff9852ad1fa828046d21a5c4b9611657825004
nate mathews <njm3308@rit.edu>
2017-01-27 20:29:59 -0500

    Added comments to list interactive. List interactive generates pages for viewing individual events now (albeit the display is a place holder).

--- Database/Schemas/serverEventsSchema.js
+++ Database/Schemas/serverEventsSchema.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Server's events data
 module.exports = new mongoose.Schema({
 		title: {type: String, required: false, default: "(no title)"},
-    description: {type: String, required: false},
+    	description: {type: String, required: false},
 		start: {type: Date, required: false },
 		end: {type: Date, default: this.start },
 		isStarted: {type: Boolean, default: false},
--- Modules/ServerEventsModules/ListInteractive.js
+++ Modules/ServerEventsModules/ListInteractive.js
@@ -2,11 +2,12 @@ const async = require("async");
 
 module.exports = (bot, db, winston, serverDocument, msg) => {
     const hasDeletePerm = msg.channel.permissionsOf(bot.user.id).has("manageMessages");
-    const max_page_size = 7;
+    const max_page_size = 7;    // maximum events viewable per page
 
     let tmp = serverDocument.gameEvents;
     let pages = [];
 
+    // generate a nested array representing viewable pages of events
     let new_page = [];
     for( let i = 0; i<tmp.length; i++ ) {
         new_page.push(tmp[i]);
@@ -15,78 +16,103 @@ module.exports = (bot, db, winston, serverDocument, msg) => {
             new_page = [];              // reset page,
         }
     }
-    if(new_page.length>0)
-        pages.push(new_page);
+    if(new_page.length>0)           // add the last page if it wasn't filled
+        pages.push(new_page);       // and added in the previous loop
 
-    pages.push([])                          // weird fix, add an extra empty page
+    pages.push([]);                         // weird fix, add an extra empty page
     let real_page_size = pages.length-1;    // set to never logically access it
 
-    let current_page_no = 1;
-
+    // function that is used to generate
     const getPage = (page_no) => {
         let current_page = pages[page_no-1];
 
-        let description = "";
+        let page_content = "";
         for (let i=0; i<current_page.length; i++) {
-            description += `\`\`[${i+1}]\`\` **${current_page[i].title}**\n`;
+            page_content += `\`\`[${i+1}]\`\` **${current_page[i].title}**\n`;
         }
-
-        description += "\n";
-        
+        page_content += "\n";
         if(real_page_size>1 && page_no<real_page_size )
-            description += `\`\`[${max_page_size+1}]\`\` **Go to next page**\n`;
+            page_content += `\`\`[${max_page_size+1}]\`\` **Go to next page**\n`;
         if(page_no>1)
-            description += `\`\`[${max_page_size+2}]\`\` **Return to previous page**\n`;
+            page_content += `\`\`[${max_page_size+2}]\`\` **Return to previous page**\n`;
+        page_content += `\`\`[cancel]\`\` **Exit view**\n`;
 
-        description += `\`\`[cancel]\`\` **Exit view**\n`;
+        let footer_content = `page ${page_no}/${real_page_size}`;
 
-        return {embed: {description: description, footer: {text: `page ${page_no}/${real_page_size}`}}}
+        return {embed: {description: page_content, footer: {text: footer_content}}}
     };
 
-    let embed = getPage(current_page_no);
-    let cancel = true;
-    let usr_err = false;
+    // function which is used to generate the page view of a single event entry
+    const getEventPage = (event_no) => {
+        let event = pages[current_page_no][event_no-1];
+        let page_content = "" +
+            `Title: ${event.title}\n` +
+            `Start: ${event.start}\n` +
+            `End: ${event.end}\n` +
+            `Description: ${event.description}\n` +
+            `Max Attendees: ${event.maxAttendees}\n` +
+            `Member: ${event.members}\n\n` +
+            `\`\`[back]\`\` to return to event list\n` +
+            `\`\`[cancel]\`\` to quit the interactive`;
 
+        let footer_content = `event ${event_no}/${pages[current_page_no].length}`;
+
+        return {embed: {description: page_content, footer: {text: footer_content}}}
+    };
+
+    let current_page_no = 1;
+    let embed = getPage(current_page_no);
+    let cancel = false;
+    let view_event = false;
     let err_msg;
 
     async.whilst(() => {
-            return cancel;
+            return !cancel;
         },
         (callback) => {
             msg.channel.createMessage(embed).then(bot_message => {
                 bot.awaitMessage(msg.channel.id, msg.author.id, usr_message => {
                     bot.removeMessageListener(msg.channel.id, msg.author.id);
 
-                    if (usr_err) {
-                        usr_err = false;
-                    }
-
                     let usr_input = usr_message.content.trim();
 
-                    // get event
-                    if (usr_message.content.trim() <= pages[current_page_no].length && usr_input > 0) {
-                        // TODO
+                    if(!view_event) {       // if in event view
+                        // return to event list page
+                        if(usr_input == "back") {
+                            view_event = false;
+                            embed = getPage(current_page_no);
+                        }
+                        // exit
+                        else if(usr_input == "cancel") {
+                            cancel = true;
+                        }
                     }
-                    // go to next page
-                    else if (usr_input == max_page_size+1 && current_page_no<real_page_size) {
-                        current_page_no++;
-                        embed = getPage(current_page_no);
-                    }
-                    // go to previous page
-                    else if (usr_input == max_page_size+2 && current_page_no>1) {
-                        current_page_no--;
-                        embed = getPage(current_page_no);
-                    }
-                    // exit interactive
-                    else if(usr_input.toLowerCase() == "cancel") {
-                        cancel = false;
-                    }
-                    // error
-                    else {
-                        err_msg = msg.channel.createMessage("That's not an option! Please try again.").then((msg)=> {
-                            setTimeout(()=>{msg.delete();},10000)
-                        });
-                        usr_err = true;
+                    else {      // otherwise
+                        // get event
+                        if (usr_message.content.trim() <= pages[current_page_no].length && usr_input > 0) {
+                            embed = getEventPage(usr_input);
+                            view_event = true;
+                        }
+                        // go to next page
+                        else if (usr_input == max_page_size+1 && current_page_no<real_page_size) {
+                            current_page_no++;
+                            embed = getPage(current_page_no);
+                        }
+                        // go to previous page
+                        else if (usr_input == max_page_size+2 && current_page_no>1) {
+                            current_page_no--;
+                            embed = getPage(current_page_no);
+                        }
+                        // exit interactive
+                        else if(usr_input.toLowerCase() == "cancel") {
+                            cancel = true;
+                        }
+                        // error
+                        else {
+                            err_msg = msg.channel.createMessage("That's not an option! Please try again.").then((msg)=> {
+                                setTimeout(()=>{msg.delete();},10000)
+                            });
+                        }
                     }
 
                     bot_message.delete();
