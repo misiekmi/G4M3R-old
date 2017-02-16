bf9f2705dc80b03f1c8a29b2b508163af40392b9
nate mathews <njm3308@rit.edu>
2017-02-16 15:07:56 -0500

    fix: an error in AdminOrAuthor was causing crashing

--- Modules/Events/AdminOrAuthor.js
+++ Modules/Events/AdminOrAuthor.js
@@ -1,10 +1,12 @@
+
+/// simple synchronous function which determines if a user has the rights to modify an event
 module.exports = (serverDocument, eventDocument, userDocument) => {
     let userID = userDocument._id;
     if(userID===eventDocument._author) {
         return true;
     } else {
         for(let i=0; i<serverDocument.config.admins.length; i++) {
-            if(serverDocument.config.admins.length[i]._id===userID) {
+            if(serverDocument.config.admins[i]._id===userID) {
                 return true;
             }
         }
