9a6cfba9eaa44e75e5c2f4bd1ae56aee1c4be6ee
pedall <peterdodell@icloud.com>
2017-02-18 22:34:57 +0100

    style: no about embed, no author help

--- Commands/PM/help.js
+++ Commands/PM/help.js
@@ -2,11 +2,12 @@ module.exports = (bot, db, config, winston, userDocument, msg, suffix) => {
         if (suffix) {
             const getCommandHelp = (name, type, usage, description, examples) => {
                     let title_content, page_content, footer_content;
-                    let embed_author = {
+                    /*                    
+					let embed_author = {
                         name: bot.user.username + ` >>> Help for ${type} command [${name}]`,
                         icon_url: bot.user.avatarURL,
                         url: "https://github.com/pedall/G4M3R"
-                    };
+                    };*/
 
                     let msg_color = 0xffffff; //white color
                     title_content = ``;
@@ -18,7 +19,7 @@ module.exports = (bot, db, config, winston, userDocument, msg, suffix) => {
                     
 					footer_content = `Please go to the Wiki for more details`;
 
-                    return { embed: { author: embed_author, color: msg_color, description: page_content, footer: { text: footer_content } } };
+                    return { embed: { color: msg_color, description: page_content, footer: { text: footer_content } } };
 			};
 
 		const info = [];
@@ -46,15 +47,13 @@ module.exports = (bot, db, config, winston, userDocument, msg, suffix) => {
 		let title_content, page_content, footer_content, fields_content;
 		
 		let msg_color = 0xffffff; //white color
+		/*
 		let embed_author = {
 			name: bot.user.username + ` >>> Help for G4M3R's commands`,
 			icon_url: bot.user.avatarURL,
 			url: "https://github.com/pedall/G4M3R"
 		};
 
-		footer_content = `To get an overview type the respective command (e.g. commands all)`;
-		
-		/* not needed atm
 		title_content = ``; 
 		page_content = `\n
 		\`commands\` (to get all command categories)\n
@@ -62,7 +61,7 @@ module.exports = (bot, db, config, winston, userDocument, msg, suffix) => {
 		\`commands pm\` (to get all commands in PM mode)\n
 		\`commands all\` (to get all commands at once)`;
 		*/
-
+		footer_content = `To get an overview type the respective command (e.g. commands all)`;
 		fields_content = [
 			{
 				name: "commands",
@@ -88,7 +87,6 @@ module.exports = (bot, db, config, winston, userDocument, msg, suffix) => {
 
         msg.channel.createMessage({
 			 embed: {
-				author: embed_author,
 				color: msg_color,
 				fields: fields_content,
 				footer: {
