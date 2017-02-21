463cf3fb2ed6948010de4e3ccdbdf442a82e1fa2
nate mathews <njm3308@rit.edu>
2017-02-21 11:35:31 -0500

    fix: myevents works again

--- Commands/Public/myevents.js
+++ Commands/Public/myevents.js
@@ -1,18 +1,20 @@
 const list = require("./../../Modules/Events/InteractiveLoop.js");
 const EventViewer = require("./../../Modules/Events/EventsViewer");
+const QueryHelper = require('../../Modules/Events/EventsQueryHelper');
 
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
 
     let viewer;
-
-    const page_size = 3;
+    const page_size = 4;
 
     if(!suffix) {
-        let filter = {};
-        filter._author = msg.author.id;
-
-        viewer = new EventViewer(serverDocument, page_size, filter);
-        list(bot, db, winston, serverDocument, msg, viewer, viewer.getPageView(1));
+        // create a filter from using the author's id
+        let filter = {_author: userDocument._id};
+        // setup the menu
+        QueryHelper.findFilteredServerEvents(db, serverDocument._id, filter).then((eventDocuments) => {
+            viewer = new EventViewer(db, serverDocument, eventDocuments, userDocument, msg.member, page_size);
+            list(bot, db, winston, serverDocument, msg, viewer, viewer.getPageView(1));
+        });
     }
     else {
         msg.channel.createMessage("Do not use a suffix please!");
