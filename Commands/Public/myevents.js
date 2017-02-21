02d98a1e75ce9069a913c771a5fc2861bac17875
pedall <peterdodell@icloud.com>
2017-02-21 13:37:55 +0100

    fix: myevents working again with queryhelper logic

--- Commands/Public/myevents.js
+++ Commands/Public/myevents.js
@@ -1,5 +1,6 @@
 const list = require("./../../Modules/Events/InteractiveLoop.js");
 const EventViewer = require("./../../Modules/Events/EventsViewer");
+const QueryHelper = require('../../Modules/Events/EventsQueryHelper');
 
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
 
@@ -11,10 +12,12 @@ module.exports = (bot, db, config, winston, userDocument, serverDocument, channe
         let filter = {};
         filter._author = msg.author.id;
 
-        viewer = new EventViewer(serverDocument, page_size, filter);
-        list(bot, db, winston, serverDocument, msg, viewer, viewer.getPageView(1));
-    }
-    else {
+        QueryHelper.findFilteredServerEvents(db, serverDocument._id, filter).then((eventDocuments) => {
+            viewer = new EventViewer(bot, msg, db, serverDocument, eventDocuments, userDocument, msg.member, page_size);
+            list(bot, db, winston, serverDocument, msg, viewer, viewer.getPageView(1));
+        });
+
+    } else {
         msg.channel.createMessage("Do not use a suffix please!");
     }
 };
