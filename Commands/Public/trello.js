45473afa4dcc2c6147ec47b65f4480d9466dc856
pedall <peterdodell@icloud.com>
2017-02-21 11:36:34 +0100

    text: new trello embed

--- Commands/Public/trello.js
+++ Commands/Public/trello.js
@@ -1,16 +1,19 @@
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix) => {
-let strTitle = "https://trello.com/b/qBjbOh48/g4m3r";
+let strLink = "https://trello.com/b/qBjbOh48/g4m3r";
 
 		msg.channel.createMessage({
 			embed: {
 				author: {
-					name: bot.user.username,
-					icon_url: bot.user.avatarURL,
+					name: "Trello for G4M3R",
+					icon_url: "http://frid.li/YaBD2",
 					url: "https://github.com/pedall/G4M3R/issues"
 				},
 				color: 0x9ECDF2,
-				title: "Trello for G4M3R",
-				description: strTitle
+				title: "Please look here for already noted ideas / suggestions / bugs",
+				description: strLink,
+				thumbnail: {
+					url: "http://frid.li/YaBD2"
+				}
 			}
 		});
 };
