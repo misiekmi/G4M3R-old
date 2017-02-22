e99c9cfc5ccb128a7f9d50a6fec6a9f22e83c2b4
nate mathews <njm3308@rit.edu>
2017-02-21 21:29:59 -0500

    test: fix?

--- Web/WebServer.js
+++ Web/WebServer.js
@@ -100,15 +100,6 @@ const getChannelData = (svr, type) => {
     });
 };
 
-const getChannel = (svr, id) => {
-    for (let i = 0; i < svr.channels.length; i++) {
-        console.log(svr.channels[i].name);
-        if (svr.channels[i].id == id) {
-            return svr.channels[i];
-        }
-    }
-};
-
 const getRoleData = svr => {
     return svr.roles.filter(role => {
         return role.name != "@everyone" && role.name.indexOf("color-") != 0;
@@ -3051,11 +3042,10 @@ module.exports = (bot, db, auth, config, winston) => {
                 serverData: {
                     name: svr.name,
                     id: svr.id,
-                    icon: svr.iconURL || "/static/img/discord-icon.png"
+                    icon: svr.iconURL || "/static/img/discord-icon.png",
+                    announce_channel: serverDocument.event_channels.announce
                 },
                 channelData: getChannelData(svr),
-                announceChannel: getChannel(svr, serverDocument.event_channels.announce),
-                advertsChannel: getChannel(svr, serverDocument.event_channels.adverts),
                 currentPage: req.path
             });
         });
