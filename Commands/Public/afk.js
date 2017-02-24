ad00e13f3d223cf0c8d9ca5d0ad619fc134bb1fe
pedall <peterdodell@icloud.com>
2017-02-24 08:16:48 +0100

    fix: public afk working again

--- Commands/Public/afk.js
+++ Commands/Public/afk.js
@@ -2,16 +2,23 @@ module.exports = (bot, db, config, winston, userDocument, serverDocument, channe
 	if(suffix) {
 		if(suffix==".") {
 			memberDocument.afk_message = null;
-			msg.channel.createMessage("Welcome back! 🎊 I removed your AFK message");
+            msg.channel.createMessage("Welcome back! I removed your AFK message");
 		} else {
 			memberDocument.afk_message = suffix;
-			msg.channel.createMessage(`Alright, I'll show that when someone mentions you on this server. 👌 Use \`${bot.getCommandPrefix(msg.guild, serverDocument)}${commandData.name} .\` to remove it`);
+            msg.channel.createMessage(`Alright, I'll show that when someone mentions you on this server. 👌 Use \`${bot.getCommandPrefix(msg.guild, serverDocument)}${commandData.name} .\` to remove it`)
 		}
-	} else {
+        serverDocument.save(err => {
+            if (err) {
+                winston.error("Failed to save server data for public afk command");
+            }
+        });
+
+    } else {
 		if(memberDocument.afk_message) {
 			msg.channel.createMessage(`You have the AFK message \`${memberDocument.afk_message}\` set on this server 💭`);
 		} else {
 			msg.channel.createMessage("You don't have an AFK message set on this server ⌨️");
 		}
 	}
+
 };
