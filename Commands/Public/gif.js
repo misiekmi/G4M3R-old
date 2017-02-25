1706486d8832e817ab980a63ba2920d81d7dd479
pedall <peterdodell@icloud.com>
2017-02-25 01:10:29 +0100

    style: gif de-embed, pics landing page

--- Commands/Public/gif.js
+++ Commands/Public/gif.js
@@ -1,46 +1,17 @@
 const getGIF = require("./../../Modules/GiphySearch.js");
 
 module.exports = (bot, db, config, winston, userDocument, serverDocument, channelDocument, memberDocument, msg, suffix, commandData) => {
-	if(suffix) {
+    if(suffix) {
         getGIF(suffix, (serverDocument.config.moderation.isEnabled && serverDocument.config.moderation.filters.nsfw_filter.isEnabled && serverDocument.config.moderation.filters.nsfw_filter.disabled_channel_ids.indexOf(msg.channel.id) == -1) ? "pg-13" : "r", function(url) {
             if(url) {
-                msg.channel.createMessage({
-                    embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/pedall/G4M3R"
-                        },
-                        color: 0x00FF00,
-                        description: `You've called for a GIF, and I found [this](${url})`
-                    }
-                });
+                msg.channel.createMessage(url);
             } else {
                 winston.warn(`No GIFs found for '${suffix}'`, {svrid: msg.guild.id, chid: msg.channel.id, usrid: msg.author.id});
-                msg.channel.createMessage({
-                    embed: {
-                        author: {
-                            name: bot.user.username,
-                            icon_url: bot.user.avatarURL,
-                            url: "https://github.com/pedall/G4M3R"
-                        },
-                        color: 0xFF0000,
-                        description: "The Internet has run out of memes (╯°□°）╯︵ ┻━┻"
-                    }
-                });
+                msg.channel.createMessage("The Internet has run out of memes **(╯°□°）╯︵ ┻━┻**");
             }
         });
-	} else {
-		msg.channel.createMessage({
-			embed: {
-                author: {
-                    name: bot.user.username,
-                    icon_url: bot.user.avatarURL,
-                    url: "https://github.com/pedall/G4M3R"
-                },
-                color: 0xFF0000,
-				description: `How am I able to guess what GIF you want? Please use the syntax \`${bot.getCommandPrefix(msg.guild, serverDocument)}${commandData.name} <terms to search>\``
-			}
-		});
-	}
+    } else {
+        msg.channel.createMessage(`How am I able to guess what GIF you want? Please use the syntax \`` +
+                                    `${bot.getCommandPrefix(msg.guild, serverDocument)}${commandData.name} <terms to search>\``);
+    }
 };
