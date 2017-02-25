aecbf936316b05b27749359f4b6eafb01c8f71d9
notem <njm3308@rit.edu>
2017-02-24 19:27:13 -0500

    fix: edits to the event start DM

--- Modules/Events/EventTimeChecker.js
+++ Modules/Events/EventTimeChecker.js
@@ -9,57 +9,30 @@ function eventStart(bot, winston, db, eventDocument) {
         db.servers.findOne({_id: eventDocument._server}).then(serverDocument => {
             let mentioned = [];         // list of users to mention on the server announcement
             let promises = [];
-            let username = bot.getUserOrNickname(eventDocument._author, eventDocument._server);
-            let embed_color = parseInt(config.colors.blue, 16);
-            let embed_title = "", attendeesNames = "", hasAttendees = false;
-            let footer_text = {text: ""};
-            let embed_author = {
-                name: `[${eventDocument._no}] created by ${username}`,
+
+            let svr = bot.guilds.find(guild=>{return guild.id === serverDocument._id});
+            let embed = {};
+            embed.color = parseInt(config.colors.blue, 16);
+            embed.author = {
+                name: `[${eventDocument._no}] created by ${bot.getUserOrNickname(eventDocument._author, svr)}`,
                 icon_url: "http://frid.li/sSVIJ"
             };
+            embed.footer = {text: `Be fair and participate in the events you join`};
 
-            if (eventDocument.attendees.length > 0) {
-                for (let i = 0; i < eventDocument.attendees.length; i++) {
-                    if (i > 14) { //showing max 15 attendees
-                        break;
-                    } else {
-                        if (i % 2 === 1) {
-                            attendeesNames += `\`${bot.getUserOrNickname(eventDocument.attendees[i]._id, eventDocument._server)}\`\n`;
-                        } else {
-                            attendeesNames += `\`${bot.getUserOrNickname(eventDocument.attendees[i]._id, eventDocument._server)}\`, `;
-                        }
-                        //attendeesNames += `<@`+eventDocument.attendees[i]._id+`>, `;
-                        hasAttendees = true;
-                    }
-                }
-            }
-            let tag_content = "";
-            if (eventDocument.tags.length > 0) {
-                tag_content = "" +
-                    `${eventDocument.tags.join(`, `)}`;
-            } else {
-                tag_content = "" +
-                    `no tags defined`;
-            }
-            embed_title = `The event \`${eventDocument.title}\` you joined starts now!`;
-            let embed_fields = [{
+            embed.title = `The event you joined \`${eventDocument.title}\` has just started on ${svr.name}!`;
+            embed.fields = [{
                 name: `Description`,
                 value: `${eventDocument.description}`,
                 inline: false
             }, {
+                name: `End Time`,
+                value: `${moment(eventDocument.end).format(`${config.moment_date_format}`)}`,
+                inline: true
+            }, {
                 name: `Attendees`,
                 value: `[${eventDocument.attendees.length}/${eventDocument.attendee_max}]`,
                 inline: true
-            }, {
-                name: `Attendees joined`,
-                value: `${hasAttendees ? `${attendeesNames}` : `(no attendees)`}`,
-                inline: true
-            }, {
-                name: `Tags`,
-                value: `${tag_content}`,
-                inline: false
             }];
-            footer_text = {text: `Be fair and participate in the events you join`};
 
             for(let i=0; i<eventDocument.attendees.length; i++) {
                 promises.push(db.users.findOne({_id: eventDocument.attendees[i]._id}).then(userDocument => {
@@ -69,15 +42,7 @@ function eventStart(bot, winston, db, eventDocument) {
                         }
                         if(userDocument.event_notifications!==2) {  // handles levels 1 and 3
                             //let msg_content = `**Event Notification:** Event \`\`${eventDocument.title}\`\` has begun`;
-                            let msg_content = {
-                                embed: {
-                                    author: embed_author,
-                                    color: embed_color,
-                                    title: embed_title,
-                                    fields: embed_fields,
-                                    footer: footer_text
-                                }
-                            };
+                            let msg_content = {embed: embed};
 
                             bot.getDMChannel(userDocument._id).then(privateChannel => {
                                 return privateChannel.createMessage(msg_content)
@@ -88,6 +53,7 @@ function eventStart(bot, winston, db, eventDocument) {
                     }
                 }));
             }
+
             Promise.all(promises).then(()=> {
                 if(serverDocument.event_channels.announce) {
                     let msg_content = `Event \`\`${eventDocument.title}\`\` has begun!\n`;
